1e2b3210e1530384f8587ea139723d663c6ba457
Rubens Pessoa De Barros Filho <3586658+rubenspessoa@users.noreply.github.com>
2021-04-16 17:09:12 +0200

    finished Moonshot

---
+++ hacking-with-swift/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  astronaut.swift
+//  Moonshot
+//
+//  Created by Rubens Pessoa De Barros Filho on 16.04.21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- hacking-with-swift/Moonshot/Moonshot/ContentView.swift
+++ hacking-with-swift/Moonshot/Moonshot/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: MissionView(mission: mission, astronauts: astronauts)) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
+        }
+
     }
 }
 
