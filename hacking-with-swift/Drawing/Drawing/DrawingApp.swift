b5e0c628839bdb5829bff41d6d4e56b09126e2db
Rubens Pessoa De Barros Filho <3586658+rubenspessoa@users.noreply.github.com>
2021-05-03 11:51:35 +0200

    add Drawing project

---
+++ hacking-with-swift/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Rubens Pessoa De Barros Filho on 29.04.21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
